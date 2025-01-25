16475f399a1649f8ce072bccab8f5c887235e2d3
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-01-25 22:29:51 +0900

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,10 @@ import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 console.info("%c I am api.js", "font-size: 2rem;");
 
+const apiURL = "/choreo-apis/gut-memo/backend/v1"
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 })
 
 api.interceptors.request.use(
