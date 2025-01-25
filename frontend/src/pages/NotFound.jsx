dbd4278f7092c5c5569e317f7fd02ab327b75c0f
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2025-01-25 21:13:28 +0900

    First commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import "../styles/NotFound.css"
+
+function NotFound() {
+  return <>
+    <div className="message">
+      <h1>404 !Found</h1>
+      <p>It seems the page has been cease</p>
+      <p>Come back again later, might going to appear, maybe?</p>
+    </div>
+  </>
+}
+
+export default NotFound;
